07b7aa4ecd0ab7ff6e45ed794c2379ccc935e253
imagire <imagire@gmail.com>
2025-12-19 21:21:01 +0900

    solve

---
+++ src/1 Ray Tacing 101/Assets/NewURPRenderFeature.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+
+public class NewURPRenderFeature : ScriptableRendererFeature
+{
+    [SerializeField] NewURPRenderFeatureSettings settings;
+    NewURPRenderFeaturePass m_ScriptablePass;
+
+    /// <inheritdoc/>
+    public override void Create()
+    {
+        m_ScriptablePass = new NewURPRenderFeaturePass(settings);
+
+        // Configures where the render pass should be injected.
+        // ■レンダリングパスを挿入する場所を設定
+//      m_ScriptablePass.renderPassEvent = RenderPassEvent.AfterRenderingOpaques; // ■差し替え
+        m_ScriptablePass.renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+
+        // You can request URP color texture and depth buffer as inputs by uncommenting the line below,
+        // URP will ensure copies of these resources are available for sampling before executing the render pass.
+        // Only uncomment it if necessary, it will have a performance impact, especially on mobiles and other TBDR GPUs where it will break render passes.
+        // ■以下の行のコメントを外すことで、URPカラーテクスチャと深度バッファを入力として要求できます。
+        // ■URPは、レンダリングパスを実行する前に、これらのリソースのコピーがサンプリング可能であることを保証します。
+        // ■必要な場合のみコメントを外してください。特にモバイル端末やTBDR GPUではパフォーマンスに影響し、レンダリングパスが破綻する可能性があります。
+        //m_ScriptablePass.ConfigureInput(ScriptableRenderPassInput.Color | ScriptableRenderPassInput.Depth);
+
+        // You can request URP to render to an intermediate texture by uncommenting the line below.
+        // Use this option for passes that do not support rendering directly to the backbuffer.
+        // Only uncomment it if necessary, it will have a performance impact, especially on mobiles and other TBDR GPUs where it will break render passes.
+        // ■以下の行のコメントを外すことで、URPに中間テクスチャへのレンダリングを要求できます。
+        // ■バックバッファへの直接レンダリングをサポートしないパスにこのオプションを使用してください。
+        // ■必要な場合のみコメントを外してください。特にモバイル端末やTBDR GPUではパフォーマンスに影響し、レンダリングパスが破綻する可能性があります。
+        //m_ScriptablePass.requiresIntermediateTexture = true;
+    }
+
+    // ■ 追加(ASの削除の為)
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing)
+        {
+            m_ScriptablePass?.Cleanup();
+            m_ScriptablePass = null;
+        }
+    }
+
+
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    // ■ここで、レンダラーに1つまたは複数のレンダリングパスを挿入できます。
+    // ■このメソッドは、カメラごとに1回、レンダラーを設定する際に呼び出されます。
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        // ■追加: シーンビューとプレビューでは実行しない
+        if (renderingData.cameraData.isSceneViewCamera || renderingData.cameraData.isPreviewCamera) return;
+
+        renderer.EnqueuePass(m_ScriptablePass);
+    }
+
+    // Use this class to pass around settings from the feature to the pass
+    // ■このクラスを使用して、フィーチャーからパスに設定を渡します
+    [Serializable]
+    public class NewURPRenderFeatureSettings
+    {
+        public RayTracingShader rayTracingShader = default!;// ■追加
+    }
+
+    class NewURPRenderFeaturePass : ScriptableRenderPass
+    {
+        readonly NewURPRenderFeatureSettings settings;
+
+        RayTracingShader rayTracingShader;// ■ 追加
+        RayTracingAccelerationStructure rayTracingAccelerationStructure;// ■ 追加
+
+
+        public NewURPRenderFeaturePass(NewURPRenderFeatureSettings settings)
+        {
+            this.settings = settings;
+
+            base.profilingSampler = new ProfilingSampler("NewURPRenderFeaturePass");
+            rayTracingShader = settings.rayTracingShader;// ■ 追加
+        }
+
+        // ■ 追加
+        public void Cleanup()
+        {
+            rayTracingAccelerationStructure?.Dispose();
+        }
+
+
+        // This class stores the data needed by the RenderGraph pass.
+        // It is passed as a parameter to the delegate function that executes the RenderGraph pass.
+        // ■このクラスは、RenderGraphパスに必要なデータを格納します。
+        // ■RenderGraphパスを実行するデリゲート関数にパラメーターとして渡されます。
+        private class PassData
+        {
+            public RayTracingShader rayTracingShader;// ■ 追加: レイトレシェーダー
+            public TextureHandle output_ColorTexture;// ■ 追加: レイトレ結果を書き出すテクスチャ
+            public TextureHandle camera_ColorTarget;// ■ 追加: カメラのカラーテクスチャ
+            public RayTracingAccelerationStructure rayTracingAccelerationStructure;// ■ 追加: AS
+
+            public Camera camera;// ■ 追加: カメラ
+        }
+
+        // This static method is passed as the RenderFunc delegate to the RenderGraph render pass.
+        // It is used to execute draw commands.
+        // ■この静的メソッドは、RenderGraphレンダーパスにRenderFuncデリゲートとして渡されます。
+        // ■描画コマンドを実行するために使用されます.
+//      static void ExecutePass(PassData data, RasterGraphContext context)
+        static void ExecutePass(PassData data, UnsafeGraphContext context)// ■差し替え
+        {
+            // ■ 追加
+            // UnsafeGraphContext からネイティブの CommandBuffer（GPUの命令を溜めるバッファ）を取得
+            CommandBuffer native_cmd = CommandBufferHelpers.GetNativeCommandBuffer(context.cmd);
+            // レイトレシェーダーパスを設定
+            native_cmd.SetRayTracingShaderPass(data.rayTracingShader, "NewURPRenderFeaturePass");
+            // レイトレシェーダーに出力テクスチャを設定
+            context.cmd.SetRayTracingTextureParam(data.rayTracingShader, 
+                Shader.PropertyToID("RenderTarget"), data.output_ColorTexture);
+            // レイトレを実行
+            context.cmd.DispatchRays(data.rayTracingShader, "MyRaygenShader", 
+                (uint)data.camera.pixelWidth, (uint)data.camera.pixelHeight, 1, data.camera);
+            // 結果をカメラに書き戻す
+            native_cmd.Blit(data.output_ColorTexture, data.camera_ColorTarget);
+        }
+
+        // RecordRenderGraph is where the RenderGraph handle can be accessed, through which render passes can be added to the graph.
+        // FrameData is a context container through which URP resources can be accessed and managed.
+        // ■RecordRenderGraphは、RenderGraphハンドルにアクセスできる場所であり、これを通じてレンダーパスをグラフに追加できます。
+        // ■FrameDataは、URPリソースにアクセスおよび管理できるコンテキストコンテナです。
+        public override void RecordRenderGraph(RenderGraph renderGraph, ContextContainer frameData)
+        {
+            const string passName = "Render Custom Pass";
+
+            // ■ 追加
+            UniversalResourceData resourceData = frameData.Get<UniversalResourceData>();
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+
+            // ■ 追加: 現在のカメラで描画されたカラーフレームバッファを取得
+            var colorTexture = resourceData.activeColorTexture;
+
+            // ■ 追加: レイトレ結果を描き出すバッファを作成
+            RenderTextureDescriptor rtdesc = cameraData.cameraTargetDescriptor;
+            rtdesc.graphicsFormat = GraphicsFormat.R16G16B16A16_SFloat;
+            rtdesc.depthStencilFormat = GraphicsFormat.None;
+            rtdesc.depthBufferBits = 0;
+            rtdesc.enableRandomWrite = true;
+            var resultTex = UniversalRenderer.CreateRenderGraphTexture(renderGraph, rtdesc, "_RayTracedColor", false);
+
+            // ■ 追加: Acceleration Structure を作成
+            if (rayTracingAccelerationStructure == null)
+            {
+                var settings = new RayTracingAccelerationStructure.Settings();
+                settings.rayTracingModeMask = RayTracingAccelerationStructure.RayTracingModeMask.Everything;
+                settings.managementMode = RayTracingAccelerationStructure.ManagementMode.Automatic;
+                settings.layerMask = 255;
+                rayTracingAccelerationStructure = new RayTracingAccelerationStructure(settings);
+
+                rayTracingAccelerationStructure.Build();// 今回は静的に構築
+            }
+
+            // This adds a raster render pass to the graph, specifying the name and the data type that will be passed to the ExecutePass function.
+            // ■これにより、グラフにラスターレンダーパスが追加され、名前とExecutePass関数に渡されるデータタイプが指定されます。
+//          using (var builder = renderGraph.AddRasterRenderPass<PassData>(passName, out var passData))
+            using (var builder = renderGraph.AddUnsafePass<PassData>(passName, out var passData))// ■差し替え
+            {
+                // Use this scope to set the required inputs and outputs of the pass and to
+                // setup the passData with the required properties needed at pass execution time.
+                // ■このスコープを使用して、パスの必要な入力と出力を設定し、
+                // ■パス実行時に必要なプロパティでpassDataを設定します。
+
+                // ■ 追加
+                passData.rayTracingShader = rayTracingShader;
+                passData.output_ColorTexture = resultTex;
+                passData.camera_ColorTarget = colorTexture;
+                passData.rayTracingAccelerationStructure = rayTracingAccelerationStructure;
+                passData.camera = cameraData.camera;
+                builder.UseTexture(passData.output_ColorTexture, AccessFlags.Write);
+                builder.UseTexture(passData.camera_ColorTarget, AccessFlags.ReadWrite);
+
+                // Make use of frameData to access resources and camera data through the dedicated containers.
+                // Eg:
+                // ■frameDataを利用して、専用のコンテナを通じてリソースとカメラデータにアクセスします。
+                // ■例：
+                // UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+
+//              UniversalResourceData resourceData = frameData.Get<UniversalResourceData>(); // ■ 削除: 上に移動
+
+                // Setup pass inputs and outputs through the builder interface.
+                // Eg:
+                // ■builderインターフェースを通じてパスの入力と出力を設定します。
+                // ■例：
+                // builder.UseTexture(sourceTexture);
+                // TextureHandle destination = UniversalRenderer.CreateRenderGraphTexture(renderGraph, cameraData.cameraTargetDescriptor, "Destination Texture", false);// ■差し替え
+
+                // This sets the render target of the pass to the active color texture. Change it to your own render target as needed.
+                // ■パスのレンダーターゲットがアクティブなカラーテクスチャに設定されます。必要に応じて独自のレンダーターゲットに変更してください。
+//              builder.SetRenderAttachment(resourceData.activeColorTexture, 0);// ■差し替え: 不要
+
+                // Assigns the ExecutePass function to the render pass delegate. This will be called by the render graph when executing the pass.
+                // ■レンダーパスデリゲートにExecutePass関数を割り当てます。これは、パスを実行する際にレンダーグラフによって呼び出されます。
+//              builder.SetRenderFunc((PassData data, RasterGraphContext context) => ExecutePass(data, context));// ■差し替え
+                builder.SetRenderFunc((PassData data, UnsafeGraphContext context) => ExecutePass(data, context));
+            }
+        }
+    }
+}
---
+++ src/2 Ray Tracing 3D/Assets/NewUnlitUniversalRenderPipelineShader.shader
@@ -0,0 +1,171 @@
+﻿Shader "Custom/NewUnlitUniversalRenderPipelineShader"
+{
+    Properties
+    {
+        [MainColor] _BaseColor("Base Color", Color) = (1, 1, 1, 1)
+        [MainTexture] _BaseMap("Base Map", 2D) = "white"
+    }
+
+    SubShader
+    {
+        Tags { "LightMode" = "NewURPRenderFeaturePass"}// ★追加
+//      Tags { "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline"}// ★削除
+
+        Pass
+        {
+            Name "NewURPRenderFeaturePass"// ★追加: SetRayTracingShaderPassで設定したパス名
+
+            HLSLPROGRAM
+
+//          #pragma vertex vert// ★削除
+//          #pragma fragment frag// ★削除
+            #pragma raytracing surface_shader// ★追加
+
+//          #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"// ★削除
+            #include "Packages/com.unity.render-pipelines.universal/Shaders/LitInput.hlsl"// ★追加
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Lighting.hlsl"// ★追加
+            #include "UnityRaytracingMeshUtils.cginc"// ★追加
+
+#if false // ★削除
+            struct Attributes
+            {
+                float4 positionOS : POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            struct Varyings
+            {
+                float4 positionHCS : SV_POSITION;
+                float2 uv : TEXCOORD0;
+            };
+
+            TEXTURE2D(_BaseMap);
+            SAMPLER(sampler_BaseMap);
+
+            CBUFFER_START(UnityPerMaterial)
+                half4 _BaseColor;
+                float4 _BaseMap_ST;
+            CBUFFER_END
+
+            Varyings vert(Attributes IN)
+            {
+                Varyings OUT;
+                OUT.positionHCS = TransformObjectToHClip(IN.positionOS.xyz);
+                OUT.uv = TRANSFORM_TEX(IN.uv, _BaseMap);
+                return OUT;
+            }
+            half4 frag(Varyings IN) : SV_Target
+            {
+                half4 color = SAMPLE_TEXTURE2D(_BaseMap, sampler_BaseMap, IN.uv) * _BaseColor;
+                return color;
+            }
+#endif
+
+            // ★以降追加
+
+            struct RayPayload
+            {
+                bool hit;
+                float3 radiance;
+            };
+            
+            struct AttributeData
+            {
+                float2 barycentrics;
+            };
+
+            RaytracingAccelerationStructure SceneAS;
+
+            struct Vertex
+            {
+                float3 position;
+                float3 normal;
+                float4 tangent;
+                float2 texCoord0;
+                float2 texCoord1;
+                float2 texCoord2;
+                float2 texCoord3;
+                float4 color;
+            };
+
+            // Unity ビルトイン関数を使って頂点データを得る関数
+            Vertex FetchVertex(uint vertexIndex)
+            {
+                Vertex v;
+                v.position  = UnityRayTracingFetchVertexAttribute3(vertexIndex, kVertexAttributePosition);
+                v.normal    = UnityRayTracingFetchVertexAttribute3(vertexIndex, kVertexAttributeNormal);
+                v.tangent   = UnityRayTracingFetchVertexAttribute4(vertexIndex, kVertexAttributeTangent);
+                v.texCoord0 = UnityRayTracingFetchVertexAttribute2(vertexIndex, kVertexAttributeTexCoord0);
+                v.texCoord1 = UnityRayTracingFetchVertexAttribute2(vertexIndex, kVertexAttributeTexCoord1);
+                v.texCoord2 = UnityRayTracingFetchVertexAttribute2(vertexIndex, kVertexAttributeTexCoord2);
+                v.texCoord3 = UnityRayTracingFetchVertexAttribute2(vertexIndex, kVertexAttributeTexCoord3);
+                v.color     = UnityRayTracingFetchVertexAttribute4(vertexIndex, kVertexAttributeColor);
+                return v;
+            }
+
+            // 重心座標での補間関数
+            float2 barycentricInterpolate2(float2 v0, float2 v1, float2 v2, float3 barycentrics)
+            {
+                return v0 * barycentrics.x + v1 * barycentrics.y + v2 * barycentrics.z;
+            }
+
+            float3 barycentricInterpolate3(float3 v0, float3 v1, float3 v2, float3 barycentrics)
+            {
+                return v0 * barycentrics.x + v1 * barycentrics.y + v2 * barycentrics.z;
+            }
+
+            float4 barycentricInterpolate4(float4 v0, float4 v1, float4 v2, float3 barycentrics)
+            {
+                return v0 * barycentrics.x + v1 * barycentrics.y + v2 * barycentrics.z;
+            }
+
+            // レイトレのヒットした位置から頂点データを補間する関数
+            Vertex InterpolateVertices(Vertex v0, Vertex v1, Vertex v2, float3 barycentrics)
+            {
+                Vertex v;
+                v.position  = barycentricInterpolate3(v0.position,  v1.position,  v2.position,  barycentrics);
+                v.normal    = barycentricInterpolate3(v0.normal,    v1.normal,    v2.normal,    barycentrics);
+                v.tangent   = barycentricInterpolate4(v0.tangent,   v1.tangent,   v2.tangent,   barycentrics);
+                v.texCoord0 = barycentricInterpolate2(v0.texCoord0, v1.texCoord0, v2.texCoord0, barycentrics);
+                v.texCoord1 = barycentricInterpolate2(v0.texCoord1, v1.texCoord1, v2.texCoord1, barycentrics);
+                v.texCoord2 = barycentricInterpolate2(v0.texCoord2, v1.texCoord2, v2.texCoord2, barycentrics);
+                v.texCoord3 = barycentricInterpolate2(v0.texCoord3, v1.texCoord3, v2.texCoord3, barycentrics);
+                v.color     = barycentricInterpolate4(v0.color,     v1.color,     v2.color,     barycentrics);
+                return v;
+            }
+
+            [shader("closesthit")]
+            void ClosestHitMain(inout RayPayload payload : SV_RayPayload, AttributeData attribs : SV_IntersectionAttributes)
+            {
+                // レイトレの交点から頂点インデックスを取得
+                uint3 triangleIndices = UnityRayTracingFetchTriangleIndices(PrimitiveIndex());
+
+                // 頂点インデックスから三角形の3頂点を取得
+                Vertex v0, v1, v2;
+                v0 = FetchVertex(triangleIndices.x);
+                v1 = FetchVertex(triangleIndices.y);
+                v2 = FetchVertex(triangleIndices.z);
+
+                // 3頂点からの情報を交点の重心座標で補間
+                float3 barycentricCoords = float3(
+                    1.0 - attribs.barycentrics.x - attribs.barycentrics.y, 
+                    attribs.barycentrics.x, 
+                    attribs.barycentrics.y);
+                Vertex v = InterpolateVertices(v0, v1, v2, barycentricCoords);
+
+                // ベースマップのテクスチャから色を取得
+                // _BaseMap などのプロパティは LitInput.hlsl を include すれば使用できる
+                float3 color = _BaseColor.rgb * SAMPLE_TEXTURE2D_LOD(_BaseMap, sampler_BaseMap, v.texCoord0, 0).rgb;
+                // 簡易ライティング計算
+                float3 normal = TransformObjectToWorldNormal(v.normal);
+                color *= lerp(0.1, 1.0, max(0, dot(normal, _MainLightPosition)));// [0.1, 1.0]の範囲
+                color = _MainLightColor * color;
+
+                payload.hit = true;
+                payload.radiance = color;
+//              payload.radiance = v.normal * 0.5 + 0.5;// 法線を色に変換して表示する場合
+            }
+            ENDHLSL
+        }
+    }
+}
